844620b76e88c8864fcd302a336e4a48652f1a77
Vitalij Skrupskij <vitskr@gmail.com>
2025-03-18 12:46:14 +0200

    Adding day2

--- src/reader.rs
+++ src/reader.rs
@@ -15,3 +15,37 @@ pub fn read(input: &'static str) -> (Vec<i32>, Vec<i32>) {
 
     (list1, list2)
 }
+
+pub fn read_2(input: &'static str) -> Vec<Vec<i32>> {
+    let result: Vec<Vec<i32>> = input
+        .lines()
+        .map(|x| {
+            x.split_whitespace()
+                .map(|y| y.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+        
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_read_2() {
+        let v1 = 
+            "7 6
+             1 2";
+
+        let result = read_2(v1);
+
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0][0], 7);
+        assert_eq!(result[0][1], 6);
+        assert_eq!(result[1][0], 1);
+        assert_eq!(result[1][1], 2);
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,2 +1,2 @@
 pub mod day1_1;
-pub mod day1_2;
+pub mod day2_1;
